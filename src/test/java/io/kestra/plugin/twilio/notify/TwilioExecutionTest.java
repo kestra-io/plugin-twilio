38d903ecd0e4677fa85217489bcf3b2ebbc9c4c0
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-11 00:52:13 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/twilio/notify/TwilioExecutionTest.java
+++ src/test/java/io/kestra/plugin/twilio/notify/TwilioExecutionTest.java
@@ -6,8 +6,9 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
@@ -20,6 +21,7 @@ import jakarta.inject.Inject;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @KestraTest
 public class TwilioExecutionTest extends AbstractTwilioTest {
 
@@ -29,7 +31,7 @@ public class TwilioExecutionTest extends AbstractTwilioTest {
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
 
-    @BeforeEach
+    @BeforeAll
     protected void init() throws IOException, URISyntaxException {
         repositoryLoader.load(Objects.requireNonNull(TwilioExecutionTest.class.getClassLoader().getResource("flows/common")));
         repositoryLoader.load(Objects.requireNonNull(TwilioExecutionTest.class.getClassLoader().getResource("flows/notify")));
@@ -43,7 +45,10 @@ public class TwilioExecutionTest extends AbstractTwilioTest {
             "twilio"
         );
 
-        String receivedData = waitForWebhookData(() -> FakeWebhookController.data, 5000);
+        String receivedData = waitForWebhookData(
+            () -> FakeWebhookController.data != null && FakeWebhookController.data.contains(failedExecution.getId()) ? FakeWebhookController.data : null,
+            5000
+        );
 
         String decodedData = URLDecoder.decode(receivedData, StandardCharsets.UTF_8);
 
